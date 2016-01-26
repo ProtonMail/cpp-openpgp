bddf7c3556221b325c35fa5b701b816ec2f31931
zhj4478 <zhj4478@gmail.com>
2016-01-26 10:45:17 -0800

    use the v3 keys

--- Android_Build/OpenPGP.Android.Wrapper/jni/include/bridge_impl/open_pgp_impl.hpp
+++ Android_Build/OpenPGP.Android.Wrapper/jni/include/bridge_impl/open_pgp_impl.hpp
@@ -76,9 +76,15 @@ namespace ProtonMail {
         std::vector<uint8_t> decrypt_attachment_with_password(const std::vector<uint8_t> & key, const std::vector<uint8_t> & data, const std::string & password);
 
         std::vector<uint8_t> get_public_key_session_key(const std::vector<uint8_t> & keyPackage, const std::string & passphrase);
+<<<<<<< HEAD
 
         std::vector<uint8_t> get_public_key_session_key_single_key(const std::vector<uint8_t> & keyPackage, const std::string & privateKey, const std::string & passphrase);
 
+=======
+        
+        std::vector<uint8_t> get_public_key_session_key_single_key(const std::vector<uint8_t> & keyPackage, const std::string & privateKey, const std::string & passphrase);
+        
+>>>>>>> use the v3 keys
         std::vector<uint8_t> get_symmetric_session_key(const std::vector<uint8_t> & keyPackage, const std::string & password);
 
         std::vector<uint8_t> get_new_public_key_package(const std::vector<uint8_t> & session, const std::string & publicKey);
