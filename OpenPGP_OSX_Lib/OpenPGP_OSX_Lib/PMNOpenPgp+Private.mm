9ce8c6da1c294758f893bcf8a0cc7f76cc4ec12c
Jonathan S <gereeter+code@gmail.com>
2016-08-03 16:59:28 -0700

    Expose the ability to read clearsigned messages

--- OpenPGP_OSX_Lib/OpenPGP_OSX_Lib/PMNOpenPgp+Private.mm
+++ OpenPGP_OSX_Lib/OpenPGP_OSX_Lib/PMNOpenPgp+Private.mm
@@ -289,6 +289,13 @@ static_assert(__has_feature(objc_arc), "Djinni requires ARC to be enabled for th
     } DJINNI_TRANSLATE_EXCEPTIONS()
 }
 
+- (nonnull NSString *)readClearsignedMessage:(nonnull NSString *)signedMessage {
+    try {
+        auto r = _cppRefHandle.get()->read_clearsigned_message(::djinni::String::toCpp(signedMessage));
+        return ::djinni::String::fromCpp(r);
+    } DJINNI_TRANSLATE_EXCEPTIONS()
+}
+
 - (int32_t)throwAnException {
     try {
         auto r = _cppRefHandle.get()->throw_an_exception();
