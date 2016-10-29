e902d2f40e9ecd4ea9f97fa8b2a360ab98d3db9a
zhj4478 <zhj4478@gmail.com>
2016-10-28 17:33:22 -0700

    md5 unit test

--- Source/tests/functional/crypto/hash/md5_tests.cpp
+++ Source/tests/functional/crypto/hash/md5_tests.cpp
@@ -13,17 +13,43 @@ namespace tests {
     namespace md5_tests {
         SUITE(md5)
         {
-            // Note: base64 works by encoding any 3 bytes as a four-byte string. Each triple is encoded independently of
-            // previous and subsequent triples. If, for a given set of input bytes, the number is not an even multiple of 3,
-            // the remaining 1 or two bytes are encoded and padded using '=' characters at the end. The encoding format is
-            // defined by IETF RFC 4648. Such padding is only allowed at the end of a encoded string, which makes it impossible
-            // to generally concatenate encoded strings and wind up with a string that is a valid base64 encoding.
-            //
-            // Since each triple of bytes is independent of others, we don't have to test particularly large sets if input data,
-            // validating that the algorithm can process at least two triples should be sufficient.
-            //
-            
             TEST(MD5_RFC_1321) {
+                //MD5 with test vectors from RFC 1321
+                {
+                    auto md5_hash = MD5("");
+                    auto md5_out = md5_hash.digest();
+                    VERIFY_ARE_EQUAL(hexlify(md5_out), "d41d8cd98f00b204e9800998ecf8427e");
+                }
+                
+                {
+                    auto md5_hash = MD5("abc");
+                    auto md5_out = md5_hash.digest();
+                    VERIFY_ARE_EQUAL(hexlify(md5_out), "900150983cd24fb0d6963f7d28e17f72");
+                }
+                
+                {
+                    auto md5_hash = MD5("message digest");
+                    auto md5_out = md5_hash.digest();
+                    VERIFY_ARE_EQUAL(hexlify(md5_out), "f96b697d7cb7938d525a2f31aaf161d0");
+                }
+                
+                {
+                    auto md5_hash = MD5("abcdefghijklmnopqrstuvwxyz");
+                    auto md5_out = md5_hash.digest();
+                    VERIFY_ARE_EQUAL(hexlify(md5_out), "c3fcd3d76192e4007dfb496cca67e13b");
+                }
+                
+                {
+                    auto md5_hash = MD5("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
+                    auto md5_out = md5_hash.digest();
+                    VERIFY_ARE_EQUAL(hexlify(md5_out), "d174ab98d277d9f5a5611c2c9f419d9f");
+                }
+                
+                {
+                    auto md5_hash = MD5("12345678901234567890123456789012345678901234567890123456789012345678901234567890");
+                    auto md5_out = md5_hash.digest();
+                    VERIFY_ARE_EQUAL(hexlify(md5_out), "57edf4a22be3c955ac49da2e2107b67a");
+                }
                 
             }
         }
