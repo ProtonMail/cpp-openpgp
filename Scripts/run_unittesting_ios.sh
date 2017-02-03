93c620502abb19e3eddf4c619b5d259fbc098d4c
zhj4478 <zhj4478@gmail.com>
2017-02-03 09:50:45 +0800

    update script

--- Scripts/run_unittesting_ios.sh
+++ Scripts/run_unittesting_ios.sh
@@ -4,7 +4,7 @@ SCRIPT_LOCATION=$(cd $(dirname $0);echo $PWD)
 
 xcodebuild \
 	-workspace ${SCRIPT_LOCATION}/../OpenPGP.xcworkspace \
-	-scheme ios_runner -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.1' \
+	-scheme ios_runner -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.2' \
 	-sdk iphonesimulator \
 	test
 
